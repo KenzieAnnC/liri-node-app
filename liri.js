74d5441c4c37b9c6c58a9838ac1ba0f194a1e404
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-05-18 18:00:55 -0500

    adds spotify song name, artist and preview link

--- liri.js
+++ liri.js
@@ -29,11 +29,13 @@ var myTwitter = 996482349847203841;
 var params = { follow: myTwitter, count: 20 };
 client.get('statuses/user_timeline/', params, function (error, tweets, response) {
   if (!error) {
+
+// loops through response object and extracts the tweet text and when the tweets were created
     for (var i = 0; i < tweets.length; i++) {
       tweets[i].text
       console.log("\n")
-      console.log(JSON.stringify(tweets[i].text, null, 2));
-      console.log(JSON.stringify(tweets[i].created_at, null, 2));
+      console.log("'" + tweets[i].text + "'");
+      console.log(tweets[i].created_at);
     }
   } else {
     throw error;
@@ -46,17 +48,36 @@ client.get('statuses/user_timeline/', params, function (error, tweets, response)
 ////////////////////////////////////////////////////////////
 
 var songName = '';
+var songArtists = [];
 
-spotify.search({type: "track", query: 'Althea', limit: 1}, function(err, data) {
+spotify.search({ type: "track", query: 'Althea', limit: 1 }, function (err, data) {
   if (err) {
     console.log(err);
   }
+
+  // loop through response object and extract song name, album and preview //
   for (var i = 0; i < data.tracks.items.length; i++) {
-    console.log("\n" + (i+1));
-    console.log("Track: " + data.tracks.items[i].name);
+    console.log("Song: " + data.tracks.items[i].name);
     console.log("Album: " + data.tracks.items[i].album.name);
+    console.log("Preview: " + data.tracks.items[i].preview_url);
+    if (data.tracks.items[i].artists.length > 1) {
+
+// loop through the response object and extract the name of the artist(s) then push the artist(s) into an array //
+      for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
+        songArtists.push(data.tracks.items[i].artists[j].name);
+      }
+// joins songArtists array into a string if there is more than one //
+      songArtists = songArtists.join(", ");
+      console.log("Artist(s): " + songArtists);
+    } else {
+      console.log("Artist: " + data.tracks.items[i].artists[0].name);
+    }
   }
-   
 });
 
 
+/////////////////////////////////////////////////////////////
+//                         OMDB                            //
+////////////////////////////////////////////////////////////
+
+var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
